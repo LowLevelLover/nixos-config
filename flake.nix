958fdc406c9dfd237d36a8f4005d34559a260ed0
Farzin Vatani <farzinvatani@outlook.com>
2025-04-11 16:31:25 +0330

    add zellij from unstable channel

--- flake.nix
+++ flake.nix
@@ -2,8 +2,8 @@
   description = "LowLevelLover's NixOS-Hyprland"; 
   	
   inputs = {
-  	nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-    #nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  	nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 	
   	#hyprland.url = "github:hyprwm/Hyprland"; # hyprland development
   	#distro-grub-themes.url = "github:AdisonCavani/distro-grub-themes";
@@ -11,22 +11,28 @@
   };
 
   outputs = 
-	inputs@{ self, nixpkgs, ... }:
+	inputs@{ self, nixpkgs-stable, nixpkgs-unstable, ... }:
 	let
     system = "x86_64-linux";
     host = "lowlevellover";
     username = "farzin";
 
-    pkgs = import nixpkgs {
+    pkgs = import nixpkgs-stable {
       inherit system;
       config = {
-      allowUnfree = true;
+        allowUnfree = true;
       };
     };
+    unstablePkgs = import nixpkgs-unstable { inherit system; };
   in
   {
+    apps.${system}.zellij = {
+        type = "app";
+        program = "${unstablePkgs.zellij}/bin/zellij";
+    };
+
   	nixosConfigurations = {
-      "${host}" = nixpkgs.lib.nixosSystem rec {
+      "${host}" = nixpkgs-stable.lib.nixosSystem rec {
         specialArgs = { 
       			inherit system;
       			inherit inputs;
