bcd7846653ddc59a77a39ac429f3d414f8900c58
Farzin Vatani <farzinvatani@outlook.com>
2025-05-08 16:51:32 +0330

    Update flake

--- configuration.nix
+++ configuration.nix
@@ -24,7 +24,13 @@ in
     ];
 
   nix.settings = {
-    download-attempts = 5;
+    download-attempts = 10;
+    # http-connections = 50;
+    stalled-download-timeout = 300;
+    connect-timeout = 60;
+    max-jobs = 1;
+    builders-use-substitutes = true;
+    http-connections = 1;
   };
 
   boot = {
